c627892dc3175e8c134bbf4eca016e06edaa6867
Cc2k1975 <reuven2k@gmail.com>
2025-09-07 10:35:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-from kivy.app import App
-from kivy.uix.label import Label
 
-class MyApp(App):
-    def build(self):
-        return Label(text="Hello Android!", font_size="32sp")
-
-if __name__ == "__main__":
-    MyApp().run()
