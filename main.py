a5036bca956746b186ab0b1254d8f284ba310666
Cc2k1975 <reuven2k@gmail.com>
2025-09-07 10:29:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.core.text import LabelBase
-
-# ודא שהקובץ NotoSansHebrew-Regular.ttf נמצא ליד הקובץ הזה
-LabelBase.register(name="NotoHeb", fn_regular="NotoSansHebrew-Regular.ttf")
 
 class MyApp(App):
     def build(self):
-        return Label(text="שלום אנדרואיד!", font_name="NotoHeb", font_size="32sp")
+        return Label(text="Hello Android!", font_size="32sp")
 
 if __name__ == "__main__":
     MyApp().run()
